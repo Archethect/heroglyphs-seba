ad433a5c771ec9012b588c7be40b4c76b52a1db5
Archethect <archethect@gmail.com>
2025-03-14 15:39:59 +0100

    sol version

--- src/interfaces/IApxETHVault.sol
+++ src/interfaces/IApxETHVault.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.28;
 
 /// @title IApxETHVault
 /// @notice Interface for the ApxETHVault contract which is a drip vault for ApxETH.
